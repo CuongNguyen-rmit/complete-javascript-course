4250112c9809d26aa0f5d250cadb6d41933ba4a1
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2023-03-07 09:40:10 +0700

    commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,80 @@
+/* /*let js = 'amazing';
+
+console.log("Jonas");
+console.log(23); 
+
+let firstName = "Jonas";
+let first = 'jonas';
+
+
+
+
+console.log(firstName);
+console.log(firstName);
+*/
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+
+console.log(typeof 'Jonas');
+
+//dynamic typing
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+// constant variable
+
+const birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+let lastName = 'Schmedtmann';
+console.log(lastName);  */
+
+
+const now = 2037;
+const ageJonas = 2037 - 1991; 
+
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas/2, 2**3);
+
+// 2**3 means 2 to the power of 3 
+
+const firstName = 'Jonas'; 
+const lastName = 'Schmedtmann';
+
+let x = 10 + 5; 
+x += 10; // x = x + 10 
+console.log(x);
+
+x *= 4; // x = x * 4 = 100
+console.log(x)
+x++;
+x--;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+
+console.log(ageSarah >= 18); 
+
+
+
+
+
+
+
+
+
+
+
+
+
